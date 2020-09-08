89feeeabbe780025729b07fd680cf84858fa0260
Neil Muller <drnlmuller@gmail.com>
2020-09-08 10:39:51 +0200

    Enable the talk items on the menu, as we're starting to accept talks

--- settings.py
+++ settings.py
@@ -33,18 +33,18 @@ WAFER_MENUS += (
         "menu": "talks",
         "label": _("Talks"),
         "items": [
-            #           {"name": "schedule", "label": _("Schedule"),
-            #            "url": reverse_lazy("wafer_full_schedule")},
-            #           {
-            #               "name": "accepted-talks",
-            #               "label": _("Accepted Talks"),
-            #               "url": reverse_lazy("wafer_users_talks"),
-            #           },
-            #           {
-            #               "name": "speakers",
-            #               "label": _("Speakers"),
-            #               "url": reverse_lazy("wafer_talks_speakers"),
-            #           },
+                       {"name": "schedule", "label": _("Schedule"),
+                        "url": reverse_lazy("wafer_full_schedule")},
+                       {
+                           "name": "accepted-talks",
+                           "label": _("Accepted Talks"),
+                           "url": reverse_lazy("wafer_users_talks"),
+                       },
+                       {
+                           "name": "speakers",
+                           "label": _("Speakers"),
+                           "url": reverse_lazy("wafer_talks_speakers"),
+                       },
         ],
     },
     {"menu": "news", "label": _("News"), "items": []},
